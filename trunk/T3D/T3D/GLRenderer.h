8f70a63c632904dd284e6d97fc51ff4041ea03b2
davjpen@gmail.com <davjpen@gmail.com@00d10f4f-4cc2-eb41-ecb5-1e8a83f99391>
2013-12-10 12:10:34 +0000

    Added 2D overlays Added unloadTexture

--- trunk/T3D/T3D/GLRenderer.h
+++ trunk/T3D/T3D/GLRenderer.h
@@ -11,10 +11,25 @@
 #ifndef GLRENDERER_H
 #define GLRENDERER_H
 
+#define WINDOW_WIDTH		1024
+#define	WINDOW_HEIGHT		640
+
+#include <list>
 #include "renderer.h"
+
 
 namespace T3D
 {
+	// Entry for simple display of text on screen. This is intended for diagnostic type display only
+	// Messages are only displayed for current frame then deleted
+	// This would be better implemented as a proper GameObject within the scene but there is currently
+	// no way to manage a 2D rendering context.
+	typedef struct {
+		Texture *texture;
+		int x,y;					// screen/window coordinates
+	} overlay2D;
+
+
 	class GLRenderer :
 		public Renderer
 	{
@@ -27,12 +42,26 @@ namespace T3D
 
 		void draw(GameObject* object);
 		
-		virtual void loadTexture(Texture *tex, bool repeat = false); // returns texture id
-		virtual void loadSkybox(std::string tex);
+		void loadTexture(Texture *tex, bool repeat = false);
+		void unloadTexture(unsigned int textureID);
+
+		void loadSkybox(std::string tex);
+
+		void add2DOverlay(Texture *texture, int x, int y);		// 2D overlay (used for on screen diagnostic messages mainly)
+		void remove2DOverlay(Texture *texture);					// remove overlay
 
 	private:
 		void drawMesh(Mesh *mesh);
 		void drawSkybox();
+
+		void showD2DOverlays();
+		void drawText();
+		void enable2D();
+		void disable2D();
+		void draw2DMesh(overlay2D *overlay);
+
+		std::list<overlay2D *> overlays;
+
 	};
 }
 
