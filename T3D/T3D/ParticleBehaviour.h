658a9de974a2ab9db7ad74f089c7fce2b4eb029d
davjpen <davjpen@gmail.com>
2014-01-01 23:28:10 +0000

    Basic particle system. This is still a work in progress.

--- T3D/T3D/ParticleBehaviour.h
+++ T3D/T3D/ParticleBehaviour.h
@@ -11,6 +11,8 @@
 #ifndef PARTICLEBEHAVIOUR_H
 #define PARTICLEBEHAVIOUR_H
 
+#include "Component.h"
+
 
 namespace T3D
 {
@@ -19,10 +21,41 @@ namespace T3D
 	    Particles are reusable
 	  \author  David Pentecost
 	  */
-	class ParticleBehaviour
+	class ParticleBehaviour  :
+		public Component
 	{
 	public:
-		ParticleBehaviour();
+		ParticleBehaviour(float lifeSpanMin, float lifeSpanMax);
+
+		virtual void start(GameObject *from);			// start or restart particle
+		bool isFinished() { return elapsed >= lifeSpan; }
+
+		void setPositionRange(float distance);
+		void setVelocity(Vector3 velocity, Vector3 variance);
+		void setAcceleration(Vector3 accel, Vector3 variance);
+		void setSpeedLimits(float min, float max);
+
+		//void setAlphaFade(float start, float end, float duration);
+
+		void update(float dt);
+
+	protected:
+		// Current state variables
+		float elapsed;			// elapsed active time
+		float lifeSpan;			// current lifespan of particle
+		Vector3 velocity;		// current motion
+		Vector3 acceleration;	// constant acceleration
+
+		// Initial state parameters
+		float lifeSpanMin;		// minimum lifetime of active particle
+		float lifeSpanMax;		// maximum lifetime of active particle
+		float startMaxDistance;	// maximum start distance from emitter
+		Vector3 velocityBase;	// initial base velocity (X,Y,Z per second)
+		Vector3 velocityVar;	// velocity variation (+/- to velocityBase)
+		Vector3 accelBase;		// base acceleration vector (X,Y,Z per second per second)
+		Vector3 accelVar;		// acceleration direction variance (+/- applied to accelBase)
+		float minSpeed;			// minimum speed when decelerating (units per second per second)
+		float maxSpeed;			// maximum speed when accelerating
 
 
 	};
