2f45fd45563932dc02835deb603e60bb729a3742
davjpen@gmail.com <davjpen@gmail.com@00d10f4f-4cc2-eb41-ecb5-1e8a83f99391>
2014-07-29 06:25:29 +0000

    - Fixed min and max frame rates - added an alternate average that allows some settling time - now uses a fixed sampling time for calculations and display - reuses 2D overlay rather than continuously replacing - log file modified to append - added a reset() method to reset gathered stats

--- trunk/T3D/T3D/PerfLogTask.h
+++ trunk/T3D/T3D/PerfLogTask.h
@@ -14,6 +14,9 @@
 #include "task.h"
 
 namespace T3D{
+
+	#define PERF_SAMPLING_PERIOD	0.15f			// calculation/update time period
+
 	class PerfLogTask :
 		public Task
 	{
@@ -27,15 +30,26 @@ namespace T3D{
 		void setDiagDisplay(bool state) { diagDisplay = state; };
 
 	protected:
-		long int frameCount;
-		float elapsedTime;
-		float dtmin;
-		float dtmax;
-		bool diagDisplay;
+		long int frameCount;	// total frame count
+		float elapsedTime;		// total elapsed time
 
-		Texture *diagOverlay;
+		float sampleElapsed;	// elapsed performance sampling period (up to PERF_SAMPLING_PERIOD)
+		float sampleFrames;		// number of frames within sampling period
+
+		// Recorded frame rates
+		float avgFrameRate;
+		float minFrameRate;
+		float maxFrameRate;
+
+		// Alternate way of calculating avergae frame rate
+		long int sampleCount;
+		float frameRateTotal;
+
+		bool diagDisplay;		// text overlay display flag
+		Texture *diagOverlay;	// last overlay texture generated
 
 		virtual void log();
+		virtual void reset();		// reset elasped time, frame count, min/max frames times
 	};
 }
 
